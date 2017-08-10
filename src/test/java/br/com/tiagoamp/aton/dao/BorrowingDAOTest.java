516464e8a714d0be0d3fec032f45bc8e1e3221b0
tiago.albuquerque <tiagoamp@>
2017-08-10 13:46:15 -0300

    Unit tests for Borrowing DAO working.

--- src/test/java/br/com/tiagoamp/aton/dao/BorrowingDAOTest.java
+++ src/test/java/br/com/tiagoamp/aton/dao/BorrowingDAOTest.java
@@ -3,6 +3,7 @@ package br.com.tiagoamp.aton.dao;
 import static org.junit.Assert.*;
 
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.After;
@@ -60,33 +61,103 @@ public class BorrowingDAOTest {
 	}
 
 	@Test
-	public void testUpdate() {
-		fail("Not yet implemented");
+	public void testUpdate_shouldUpdateEntity() throws SQLException {
+		Borrowing borrowRetrieved = insertBorrowingInDataBaseForTests();  // transient --> managed --> detached
+		Date newDate = new Date();
+		borrowRetrieved.setDateOfBorrowing(newDate);
+		
+		borrowDao.update(borrowRetrieved);
+		Borrowing borrowingAfterUpdate = borrowDao.findById(borrowRetrieved.getId());
+		assertNotNull(borrowingAfterUpdate);
+		assertEquals("'Borrowing date' should be updated.", newDate, borrowingAfterUpdate.getDateOfBorrowing());
+	}
+	
+	@Test
+	public void testDelete_shouldDeleteEntity() throws SQLException {
+		Borrowing borrowRetrieved = insertBorrowingInDataBaseForTests();  // transient --> managed --> detached
+		int id = borrowRetrieved.getId();
+		borrowDao.delete(id);
+		
+		borrowRetrieved = borrowDao.findById(id);
+		assertNull("Entity must be deleted.", borrowRetrieved);
+	}
+	
+	@Test
+	public void testFindById_shouldReturnValidOutput() throws SQLException {
+		Borrowing borrowingInserted = insertBorrowingInDataBaseForTests();
+		
+		Borrowing borrowingRetrievedById = borrowDao.findById(borrowingInserted.getId());
+		assertNotNull("Must retrieve the entity by Id.", borrowingRetrievedById);
+		assertEquals("Must retrieve the entity with same 'Id'.", borrowingInserted.getId(), borrowingRetrievedById.getId());		
 	}
 
 	@Test
-	public void testDelete() {
-		fail("Not yet implemented");
+	public void testFindByFields_allFieldsMatches_shouldReturnValidOutput() throws SQLException {
+		Borrowing borrow = insertBorrowingInDataBaseForTests();
+		
+		List<Borrowing> list = borrowDao.findByFields(borrow.getBook().getId(), borrow.getPerson().getId(), borrow.getDateOfBorrowing(), borrow.getDateOfReturn());
+		assertTrue(!list.isEmpty());
+		assertTrue(list.size() == 1);
+	}
+	
+	@Test
+	public void testFindByFields_byBookId_shouldReturnValidOutput() throws SQLException {
+		Borrowing borrow = insertBorrowingInDataBaseForTests();
+		
+		List<Borrowing> list = borrowDao.findByFields(borrow.getBook().getId(), null, null, null);
+		assertTrue(!list.isEmpty());
+		assertTrue(list.size() == 1);
+	}
+	
+	@Test
+	public void testFindByFields_byPersonId_shouldReturnValidOutput() throws SQLException {
+		Borrowing borrow = insertBorrowingInDataBaseForTests();
+		
+		List<Borrowing> list = borrowDao.findByFields(null, borrow.getPerson().getId(), null, null);
+		assertTrue(!list.isEmpty());
+		assertTrue(list.size() == 1);
+	}
+	
+	@Test
+	public void testFindByFields_byBorrowingDate_shouldReturnValidOutput() throws SQLException {
+		Borrowing borrow = insertBorrowingInDataBaseForTests();
+		
+		List<Borrowing> list = borrowDao.findByFields(null, null, borrow.getDateOfBorrowing(), null);
+		assertTrue(!list.isEmpty());
+		assertTrue(list.size() == 1);
+	}
+	
+	@Test
+	public void testFindByFields_byReturningDate_shouldReturnValidOutput() throws SQLException {
+		Borrowing borrow = insertBorrowingInDataBaseForTests();
+		
+		List<Borrowing> list = borrowDao.findByFields(null, null, null, borrow.getDateOfReturn());
+		assertTrue(!list.isEmpty());
+		assertTrue(list.size() == 1);
 	}
 
 	@Test
-	public void testFindById() {
-		fail("Not yet implemented");
+	public void testFindAll_emptyDataBase_shouldReturnEmptyList() throws SQLException {
+		List<Borrowing> list = borrowDao.findAll();
+		assertTrue("Must not retrieve entities.", list.isEmpty());
+	}
+	
+	@Test
+	public void testFindAll_shouldReturnValidOutput() throws SQLException {
+		insertBorrowingsListInDataBaseForTests();
+		
+		List<Borrowing> list = borrowDao.findAll();
+		assertNotNull("Must return entities previously inserted.", list);
+		assertEquals("Should return 3 entities.", 3, list.size());
 	}
 
 	@Test
-	public void testFindByFields() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFindAll() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testFindOpenBorrowings() {
-		fail("Not yet implemented");
+	public void testFindOpenBorrowings() throws SQLException {
+		insertBorrowingsListInDataBaseForTests();
+		
+		List<Borrowing> list = borrowDao.findOpenBorrowings();
+		assertNotNull("Must return entities previously inserted.", list);
+		assertEquals("Should return 2 entities with open borrowing.", 2, list.size());
 	}
 	
 	
@@ -110,12 +181,12 @@ public class BorrowingDAOTest {
 			List<Book> bookList = bookDao.findAll();
 			List<Person> personList = personDao.findAll();
 			List<Borrowing> borrowList = borrowDao.findAll();
-			for (Book book : bookList) {
-				bookDao.delete(book.getId());
-			}
 			for (Person person : personList) {
 				personDao.delete(person.getId());
 			}
+			for (Book book : bookList) {
+				bookDao.delete(book.getId());
+			}			
 			for (Borrowing borrow : borrowList) {
 				borrowDao.delete(borrow.getId());
 			}
@@ -127,7 +198,9 @@ public class BorrowingDAOTest {
 	private Borrowing insertBorrowingInDataBaseForTests() throws SQLException {
 		Person person = this.insertPersonToBorrowBook();
 		Book book = this.insertBookToBeBorrowed();
-		Borrowing borrow = TestHelper.getBorrowingForTest();		
+		Borrowing borrow = TestHelper.getBorrowingForTest();
+		borrow.setPerson(person);
+		borrow.setBook(book);
 		borrowDao.create(borrow);
 		List<Borrowing> list = borrowDao.findByFields(book.getId(), person.getId(), borrow.getDateOfBorrowing(), borrow.getDateOfReturn()); // getting db id
 		borrow = list.get(0); 
@@ -153,9 +226,9 @@ public class BorrowingDAOTest {
 		bookDao.create(book2);
 		bookDao.create(book3);
 
-		Borrowing borrow1 = new TestHelper().getBorrowingForTest();
-		Borrowing borrow2 = new TestHelper().getBorrowingForTest();
-		Borrowing borrow3 = new TestHelper().getBorrowingForTest();
+		Borrowing borrow1 = TestHelper.getBorrowingForTest();
+		Borrowing borrow2 = TestHelper.getBorrowingForTest();
+		Borrowing borrow3 = TestHelper.getBorrowingForTest();
 		
 		borrow1.setBook(book1);
 		borrow1.setPerson(person);
@@ -163,6 +236,7 @@ public class BorrowingDAOTest {
 		borrow2.setPerson(person);
 		borrow3.setBook(book3);
 		borrow3.setPerson(person);
+		borrow3.setDateOfReturn(new Date()); // borrowing 3 was returned
 		
 		borrowDao.create(borrow1);
 		borrowDao.create(borrow2);
